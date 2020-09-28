a3f5717bea44506cb66e73e3e73a17386053f3bf
Raskat <ka-rostislav@yandex.ru>
2020-09-28 18:00:08 +0700

    Изменения в добавлении карточки

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  // const { authorization } = req.headers;
   const { token } = req.cookies;
 
-  // if (!authorization && !authorization.startsWith('Bearer ')) {
-  //   return res
-  //     .status(401)
-  //     .send({ message: 'Необходима авторизация' });
-  // }
-
-  // const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'super-secret-key');
--- routes/cards.js
+++ routes/cards.js
@@ -5,12 +5,7 @@ const {
 } = require('../controllers/cards');
 
 cardRouter.get('/', getCards);
-cardRouter.post('/', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(new RegExp('^((http|https):\/\/)(www\.)?([\w\W]){1,}(#?)$')),
-  }).unknown(true),
-}), createCard);
+cardRouter.post('/', createCard);
 cardRouter.delete('/:cardId', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().required(),
