13446f5a6cbc0cedef84da00b548fa9b5221368d
Raskat <ka-rostislav@yandex.ru>
2020-10-01 11:02:12 +0700

    cors added

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(requestLogger); // подключаем логгер запросов
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('*', cors());
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
